e6b3b83050dcbaa9fbea750af9cc6fb0533853fc
aelimited <cloim09@gmail.com>
2024-03-05 16:38:38 +0900

    #7 ADD 상품 주문,취소 추가

--- src/main/java/jpabook/jpashop/Serivce/OrderService.java
+++ src/main/java/jpabook/jpashop/Serivce/OrderService.java
@@ -3,11 +3,14 @@ package jpabook.jpashop.Serivce;
 import jpabook.jpashop.Repository.ItemRepository;
 import jpabook.jpashop.Repository.MemberRepository;
 import jpabook.jpashop.Repository.OrderRepository;
+import jpabook.jpashop.Repository.OrderSearch;
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -52,9 +55,9 @@ public class OrderService {
         order.cancel();
     }
         /** 주문 검색 */
-    /*
+
     public List<Order> findOrders(OrderSearch orderSearch) {
              return orderRepository.findAll(orderSearch);
      }
-    */
+
 }
